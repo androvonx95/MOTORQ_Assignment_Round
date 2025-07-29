22e682ea86863ba0023c3969054ee47daad3beda
androvonx95 <androvonx95@tutamail.com>
2025-07-29 13:41:52 +0530

    pre lunch commit

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -7,9 +7,34 @@ const vehicles = require('../data/vehicles.js');
 
 
 // GET all vehicles 
-app.get('/all', (req, res) => {
+router.get('/all', (req, res) => {
     res.send( vehicles );
-})
+});
+
+router.post( '/new' , (req,res) => {
+    vin = req.body.vin;
+    model = req.body.Model; 
+    fleetID = req.body.fleetID;
+    operator = req.body.operator;
+    regStatus = req.body.regStatus;
+
+    let vehiclesObj = {
+        vin ,
+        model ,
+        fleetID ,
+        operator ,
+        regStatus
+    }
+
+    vehicles.push( vehiclesObj );
+} );
 
 
+router.get('/:id', (req, res) => {
+    vehiclesID = req.params.id;
+    vehicles.vehicles.find( (obj) => {
+        obj.vin = vehiclesID;
+    } );
+    res.send( vehicles );
+});
 module.exports = router;
